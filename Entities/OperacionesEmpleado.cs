40b95d077dded4ca8ab4bf90f72e94960bb5af90
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 14:23:51 -0300

    Creo entities para cantidad de operaciones

---
+++ Entities/OperacionesEmpleado.cs
@@ -0,0 +1,8 @@
+namespace lab_app_web_servidor_istea.Entities
+{
+  public class OperacionesEmpleado
+  {
+    public string NombreEmpleado { get; set; } = null!;
+    public int CantidadPedidos { get; set; } = 0;
+  }
+}
