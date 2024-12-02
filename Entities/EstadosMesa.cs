1efb3923afb804940b84325ff611e3c18656226c
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 13:17:59 -0300

    Creacion entities Mesa y EstadoMesa

---
+++ Entities/EstadosMesa.cs
@@ -0,0 +1,8 @@
+﻿namespace lab_app_web_servidor_istea.Entities;
+
+public class EstadosMesa : EntityWithId
+{
+  public required string Descripcion { get; set; }
+
+  public virtual ICollection<Mesa> Mesas { get; set; } = [];
+}
---
+++ Entities/Mesa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab_app_web_servidor_istea.Entities;
+
+public class Mesa : EntityWithId
+{
+  public required string Nombre { get; set; }
+
+  [ForeignKey(nameof(EstadosMesa))] public int EstadosMesaId { get; set; }
+
+  public required EstadosMesa EstadosMesa { get; set; }
+}
