3c83a18f13ee31f6fa85b3243ea5197b14e432e3
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 14:13:39 -0300

    Desarrollo de Context aplicando los DataSeed

---
+++ Database/RestauranteContext.cs
@@ -0,0 +1,39 @@
+﻿using lab_app_web_servidor_istea.Database.DataSeed;
+using Microsoft.EntityFrameworkCore;
+using lab_app_web_servidor_istea.Entities;
+
+namespace lab_app_web_servidor_istea.Database;
+
+public class RestauranteContext : DbContext
+{
+  public RestauranteContext()
+  {
+  }
+
+  public RestauranteContext(DbContextOptions<RestauranteContext> options)
+      : base(options)
+  {
+  }
+
+  public DbSet<EstadosPedido> EstadosPedidos { get; set; }
+  public DbSet<EstadosMesa> EstadosMesas { get; set; }
+  public DbSet<Comanda> Comandas { get; set; }
+  public DbSet<Empleado> Empleados { get; set; }
+  public DbSet<Mesa> Mesas { get; set; }
+  public DbSet<Pedido> Pedidos { get; set; }
+  public DbSet<Producto> Productos { get; set; }
+  public DbSet<Rol> Roles { get; set; }
+  public DbSet<Sector> Sectores { get; set; }
+  public DbSet<RegistroEmpleado> RegistroEmpleado { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfiguration(new EstadosMesaSeed());
+    modelBuilder.ApplyConfiguration(new EstadosPedidosSeed());
+    modelBuilder.ApplyConfiguration(new SectoresSeed());
+    modelBuilder.ApplyConfiguration(new MesaSeed());
+    modelBuilder.ApplyConfiguration(new ProductosSeed());
+    modelBuilder.ApplyConfiguration(new RolesSeed());
+    modelBuilder.ApplyConfiguration(new EmpleadoSeed());
+  }
+}
