0b62d3129287aafb399493c1fe0de89b7f6c2d01
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 14:38:59 -0300

    Creacion ComandaDTO junto con interface de repository

---
+++ DTO/ComandaDTO.cs
@@ -0,0 +1,7 @@
+namespace lab_app_web_servidor_istea.DTO;
+
+public class ComandaDTO
+{
+  public required string NombreCliente { get; set; }
+  public int IdMesa { get; set; }
+}
---
+++ Interfaces/IComandaRepository.cs
@@ -0,0 +1,13 @@
+﻿using lab_app_web_servidor_istea.Entities;
+using lab_app_web_servidor_istea.DTO;
+
+namespace lab_app_web_servidor_istea.Interfaces
+{
+  public interface IComandaRepository : IRepository<Comanda>
+  {
+    public Task<Comanda> ObtenerComandaPorId(int idComanda);
+    public Task<Comanda> AgregarComanda(ComandaDTO comanda);
+    public Task<Comanda> ActualizarComanda(Comanda comanda);
+  }
+
+}
