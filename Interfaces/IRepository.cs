438f73b6974475735bb9b8fb823b3194c16183bf
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 14:14:43 -0300

    Creacion interface Repository

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace lab_app_web_servidor_istea.Interfaces
+{
+  public interface IRepository<T> where T : class
+  {
+    Task<List<T>> GetAll();
+    Task<T> GetId(int id);
+    Task Add(T entity);
+    void Delete(T entity);
+    void Edit(T entity);
+  }
+}
