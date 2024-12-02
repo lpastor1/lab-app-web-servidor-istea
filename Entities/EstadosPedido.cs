591744fd025831a5e5d3701cc1c5496a47f323bb
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 13:18:20 -0300

    Creacion entities Pedido y EstadoPedido

---
+++ Entities/EstadosPedido.cs
@@ -0,0 +1,8 @@
+﻿namespace lab_app_web_servidor_istea.Entities;
+
+public class EstadosPedido : EntityWithId
+{
+  public string Descripcion { get; set; } = null!;
+
+  public virtual ICollection<Pedido> Pedidos { get; set; } = [];
+}
---
+++ Entities/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab_app_web_servidor_istea.Entities;
+
+public class Pedido : EntityWithId
+{
+  [ForeignKey(nameof(Comanda))] public int ComandaId { get; set; }
+
+  [ForeignKey(nameof(Producto))] public int ProductoId { get; set; }
+
+  [ForeignKey(nameof(EstadosPedido))] public int EstadosPedidoId { get; set; }
+  public int Cantidad { get; set; }
+  public DateTime FechaCreacion { get; set; }
+  public DateTime? FechaFinalizacion { get; set; }
+
+  public virtual required Producto Producto { get; set; }
+  public virtual required Comanda Comanda { get; set; }
+  public virtual required EstadosPedido EstadosPedido { get; set; }
+}
