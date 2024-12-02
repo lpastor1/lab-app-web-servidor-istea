9aaf124f06a1fe37914c4606f6f9c54e941da26d
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 13:18:30 -0300

    Ajusto indentacion

--- Entities/EntityWithId.cs
+++ Entities/EntityWithId.cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace lab_app_web_servidor_istea.Entities
 {
-    public abstract class EntityWithId
-    {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-    }
+  public abstract class EntityWithId
+  {
+    [Key]
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+  }
 }
