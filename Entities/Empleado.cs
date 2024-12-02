cfbfadb197178b1ce73a80971ae87efed7edfc68
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 13:36:53 -0300

    Creacion entities Empleado y RegistroEmpleado

---
+++ Entities/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab_app_web_servidor_istea.Entities;
+
+public class Empleado : EntityWithId
+{
+  [Required] public required string Nombre { get; set; }
+
+  [Required] public required string Usuario { get; set; }
+
+  [Required] public required string Password { get; set; }
+  [ForeignKey(nameof(Sector))] public int IdSector { get; set; }
+
+  [ForeignKey(nameof(Rol))] public int RoleId { get; set; }
+
+  public virtual Sector Sector { get; set; } = null!;
+
+  public virtual Rol Rol { get; set; } = null!;
+}
---
+++ Entities/RegistroEmpleado.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab_app_web_servidor_istea.Entities
+{
+  public class RegistroEmpleado : EntityWithId
+  {
+    [ForeignKey(nameof(Empleado))] public int IdEmpleado { get; set; }
+
+    public virtual required Empleado Empleado { get; set; }
+
+    public DateTime FechaHora { get; set; }
+  }
+}
