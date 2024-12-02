74c595e5348628a8e428046084e9b8af850e751a
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 12:56:00 -0300

    Agregado de enum RolTrabajador

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using lab_app_web_servidor_istea.Enums;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(RolTrabajadorEnum.Bartender.ToString());
