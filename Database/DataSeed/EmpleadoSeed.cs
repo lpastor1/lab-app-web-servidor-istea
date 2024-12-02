fa3be5af50659e59d9c8b35634789737ac15e775
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 19:10:53 -0300

    Subo ajustes en DataSeed

--- Database/DataSeed/EmpleadoSeed.cs
+++ Database/DataSeed/EmpleadoSeed.cs
@@ -9,11 +9,11 @@ public class EmpleadoSeed : IEntityTypeConfiguration<Empleado>
   public void Configure(EntityTypeBuilder<Empleado> builder)
   {
     builder.HasData(
-        new Empleado() { Id = 0, RoleId = 0, Nombre = "Enzo Francescoli", Password = "river1", IdSector = 0, Usuario = "elenzo" },
-        new Empleado() { Id = 1, RoleId = 1, Nombre = "Angel Labruna", Password = "river2", IdSector = 1, Usuario = "angelito" },
-        new Empleado() { Id = 2, RoleId = 2, Nombre = "Marcelo Gallardo", Password = "river3", IdSector = 2, Usuario = "elmunie" },
-        new Empleado() { Id = 3, RoleId = 3, Nombre = "Norberto Alonso", Password = "river4", IdSector = 3, Usuario = "elbeto" },
-        new Empleado() { Id = 4, RoleId = 4, Nombre = "Amadeo Carrizo", Password = "river5", IdSector = 3, Usuario = "elpulpo" }
+        new Empleado() { Id = 0, RolId = 0, Nombre = "Enzo Francescoli", Password = "river1", IdSector = 0, Usuario = "elenzo" },
+        new Empleado() { Id = 1, RolId = 1, Nombre = "Angel Labruna", Password = "river2", IdSector = 1, Usuario = "angelito" },
+        new Empleado() { Id = 2, RolId = 2, Nombre = "Marcelo Gallardo", Password = "river3", IdSector = 2, Usuario = "elmunie" },
+        new Empleado() { Id = 3, RolId = 3, Nombre = "Norberto Alonso", Password = "river4", IdSector = 3, Usuario = "elbeto" },
+        new Empleado() { Id = 4, RolId = 4, Nombre = "Amadeo Carrizo", Password = "river5", IdSector = 3, Usuario = "elpulpo" }
     );
   }
 }
