242d632880a66f3b43de2a33d410d32c8b488eee
lautaro.pastor <lautaro.pastor@bbva.com>
2024-12-02 12:40:48 -0300

    Creo clase abstracta EntityWithId

---
+++ Entities/EntityWithId.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab_app_web_servidor_istea.Entities
+{
+    public abstract class EntityWithId
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
